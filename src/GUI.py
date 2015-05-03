f28f5429e03d62b296cf8d5aa6184ba9c251146c
Rocco Febbo <febbo87@gmail.com>
2015-05-02 22:10:05 -0400

    make a blank window

--- src/GUI.py
+++ src/GUI.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from tkinter import *
-from SSFileManager import *
-import threading
+from tkinter import ttk
 
-IsRunning = True
-CONST_UPDATETIME = 1.0
 CONST_SIMULATION_MODE = True
 
-"""run our update code in a different thread while root.mainloop runs?"""
-def update():
-    if IsRunning:
-        threading.Timer(CONST_UPDATETIME, update).start()
-        if CONST_SIMULATION_MODE:
-            pass
-            #Data.SimUpdate()
-        else:
-            pass
-            #Data.Update()
+class UI():
 
-def main():
-    #initialize objects
-    root=Tk()
-    fileManager = FileManager()
-
-    update() #Start our thread
-    root.mainloop() #This runs untill you close the window
-
-    IsRunning = False #Stop our thread
-
-
-if __name__ == "__main__":
-    main()
+    def __init__(self, spudScale, inputTitle, orderedNames):
+        root = Tk()
+        root.title("Spud Scale")
+        root.mainloop()
