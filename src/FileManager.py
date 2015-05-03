507d4923413cec353bb3b9336fad65d5743eff4d
Brett Nelson <brett@brettnelson.org>
2015-05-02 21:41:51 -0400

    Removed main()

--- src/FileManager.py
+++ src/FileManager.py
@@ -19,25 +19,3 @@ class FileManager():
         with open(self.fileName, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, 'excel')
             writer.writerow(readout)
-
-
-def main():
-    from ConfigReader import configReader
-    #read config file
-    usrconfig = configReader()
-
-    #to initiaize values
-    fileManager = FileManager('testOutput.py')
-    fileManager.fileName = usrconfig['History']
-    #fileManager.scaleNames = usrconfig['Scales']
-
-    #to create and save a readout
-    readout = ['plotName', '12', '51', '45', '84', '32', '54', '65', '12', '45', '98']
-    fileManager.record(readout)
-
-    fileManager.readHistory()
-
-
-
-if __name__ == '__main__':
-    main()
