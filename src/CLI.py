46356259408e3207fb1d803b7b68dd186dc3f256
Brett Nelson <brett@brettnelson.org>
2015-05-07 00:41:03 -0400

    Modified for conformance with flake8

--- src/CLI.py
+++ src/CLI.py
@@ -1,7 +1,9 @@
 import SpudScale
-class CLI() :
 
-    def __init__(self) :
+
+class CLI():
+
+    def __init__(self):
         self.spudScale = SpudScale.SpudScale()
         self.title = 'SpudScale' + ' '*49 + 'University of Florida'
         self.message = ''
@@ -9,21 +11,21 @@ class CLI() :
         self.orderedNames = self.spudScale.getOrderedNames()
 
     def mainMenu(self):
-        while not self.exit :
+        while not self.exit:
             choices = {'1', '2', '3', 'Q'}
             self.printMain()
             selection = input('Please make a selection: ')
-            if selection in choices :
-                if selection == '1' :
+            if selection in choices:
+                if selection == '1':
                     self.recordMode()
-                elif selection == 'Q' :
+                elif selection == 'Q':
                     self.exit = True
-            else :
+            else:
                 self.message = 'Invalid Option'
 
-    def recordMode(self) :
+    def recordMode(self):
         returnToMain = False
-        while not returnToMain :
+        while not returnToMain:
             self.printCompDisplay()
             self.blankLines(3)
             print('Enter: a plot number to record current values')
@@ -31,10 +33,10 @@ class CLI() :
             print('       or Q to return to the main menu')
             userInput = input('Plot #: ')
             choices = {'', 'Q'}
-            if userInput in choices :
-                if userInput == 'Q' :
+            if userInput in choices:
+                if userInput == 'Q':
                     returnToMain = True
-            else :
+            else:
                 self.spudScale.record(userInput)
 
     def printCompDisplay(self):
@@ -45,35 +47,36 @@ class CLI() :
         numNames = len(self.orderedNames)
         print(self.title)
         print(' '+'_'*78)
-        print(' |{0:^10s}|{1:^10s}|{2:^54s}|'.format('Live', 'Scale','Recorded'))
+        print(' |{0:^10s}|{1:^10s}|{2:^54s}|'.format(
+            'Live', 'Scale', 'Recorded'))
         print(' |'+'-'*10+'+'+'-'*10+'+'+'-'*(10*5 + 4)+'|')
         rowVal = ['']*7
-        for i in range(0, numFive) :
-            if (i < (numFive - numCurrent)) :
+        for i in range(0, numFive):
+            if (i < (numFive - numCurrent)):
                 rowVal[0] = ''
-            else :
+            else:
                 rowVal[0] = currentValues[(i - (numFive - numCurrent))]
-            if (i < (numFive - numNames)) :
+            if (i < (numFive - numNames)):
                 rowVal[1] = 'Plot#'
-            else :
+            else:
                 rowVal[1] = self.orderedNames[(i - (numFive - numNames))]
-            for j in range(0, len(lastFiveRecorded)) :
+            for j in range(0, len(lastFiveRecorded)):
                 rowVal[(j + 2)] = lastFiveRecorded[j][i]
-            print(' |{0:^10s}|{1:^10s}|{2:^10s}|{3:^10s}|{4:^10s}|{5:^10s}|{6:^10s}|'\
-              .format(rowVal[0],rowVal[1],rowVal[2],rowVal[3],rowVal[4],rowVal[5],rowVal[6]))
+            print(' |{0:^10s}|{1:^10s}|{2:^10s}\
+                |{3:^10s}|{4:^10s}|{5:^10s}|{6:^10s}|'.format(
+                rowVal[0], rowVal[1], rowVal[2], rowVal[3],
+                rowVal[4], rowVal[5], rowVal[6]))
         self.blankLines(3)
 
-
-
-    def printCurrent(self) :
+    def printCurrent(self):
         print('Current')
 
-    def printMain(self) :
+    def printMain(self):
         print(self.title)
         self.blankLines(3)
         print('        _________                .____________             .__')
         print('       /   _____/_____  __ __  __| _/   _____/ ____ _____  |  |   ____')
-        print('       \\_____  \\\\____ \\|  |  \\/ __ |\\_____  \\_/ ___\\\\__  \\ |  | _/ __ \\')
+        print('       \\_____  \\\____ \\|  |  \\/ __ |\\_____  \\_/ ___\\\\__  \\ |  | _/ __ \\')
         print('       /        \\  |_> >  |  / /_/ |/        \\  \\___ / __ \\|  |_\\  ___/')
         print('      /_______  /   __/|____/\\____ /_______  /\\___  >____  /____/\\___  >')
         print('              \\/|__|              \\/       \\/     \\/     \\/          \\/ ')
@@ -85,11 +88,12 @@ class CLI() :
         self.blankLines(2)
         print(self.message)
 
-    def blankLines(self, lines) :
-        for line in range(0, lines) :
+    def blankLines(self, lines):
+        for line in range(0, lines):
             print('')
 
-def main() :
+
+def main():
     cli = CLI()
     cli.mainMenu()
 
